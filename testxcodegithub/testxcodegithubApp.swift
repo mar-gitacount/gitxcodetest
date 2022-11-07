0a31cc3926cf3f9f82c2e2ef4da64f1ee8f80423
市川マサル <ichikawamasaru@masarunoMacBookAir.local>
2022-11-08 01:04:20 +0900

    first commit

---
+++ testxcodegithub/testxcodegithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  testxcodegithubApp.swift
+//  testxcodegithub
+//
+//  Created by 市川マサル on 2022/11/08.
+//
+
+import SwiftUI
+
+@main
+struct testxcodegithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
